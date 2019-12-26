3fbb69e9c1d0e235551a3dfcf99fe606d80dbc23
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 00:53:31 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,17 @@
-def addition(num1, num2)
+  puts "Type 1 to add, 2 to subtract, 3 to multiply, or 4 to divide two numbers: "
+  operation_selection = gets.to_i
 
-end
+  if operation_selection == 1 
+    return "add"
+  elsif operation_selection == 2
+    return "subtract"
+  elsif operation_selection == 3
+    return "multiply"
+  elsif operation_selection == 4
+    return "divide"
+  else 
+    return "error"
 
-def subtraction(num1, num2)
-
-end
-
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
+  end
 
 end
